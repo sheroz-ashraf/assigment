ca5a07a85f20a689da4d65d4fb91c386faa58985
shehroz <123664709+sheroz-ashraf@users.noreply.github.com>
2023-05-04 21:39:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,139 @@
+Assignment-6 
+
+//**Chapter21-25 **//
+
+//Question 1
+
+// var firstName=prompt("Enter your first name");
+// var lastName=prompt("Enter your last name");
+// var fullName=firstName+" "+lastName;
+// console.log("Hello This is "+fullName);
+
+//Question 2
+
+// var mobModel=prompt("Enter you favorite phone model")
+// var length=mobModel.length;
+// console.log(length);
+
+//Question 3
+
+// var string="Pakistani";
+// var n=string.indexOf("n");
+// console.log(n)
+
+//Question 4
+
+// var word="Hello World";
+// var l=word.lastIndexOf("l");
+// console.log(l);
+
+//Question 5
+
+// var word="Pakistani";
+// var char=word.charAt(3);
+// console.log(char);
+
+//Question 6
+
+// var firstName="Danial";
+// var lastName="Ismail";
+// var fullName=firstName.concat(" ",lastName);
+// console.log(fullName);
+
+//Question 7
+
+// var word="Hyderabad";
+// var replaceWord=word.replace("Hyderabad","Islamabad");
+// console.log(replaceWord);
+
+//Question 8
+
+// var message="Ali and Sami are best friends. They play cricket and football together."
+// var newMessage=message.replaceAll("and","&");
+// console.log(newMessage )
+
+//Question 9
+
+// var str="472";
+// var num=Number("472");
+// console.log("Value :"+num)
+// console.log("type :"+typeof(num))
+// console.log("value :"+str)
+// console.log("type "+typeof(str))
+
+//Question 10
+
+// var input=prompt("Enter your input");
+// var result=input.toUpperCase()
+// console.log(result);
+
+//Question 11
+
+// var input=prompt("Enter your input");
+// var result=input.charAt(0);
+// var title=result.toUpperCase()
+// console.log(title+input.slice(1));
+
+//Question 12
+
+// var num=35.36
+// var newNum=num.toString().replace(".","")
+// console.log(newNum)
+
+//Question 13
+
+// var username = prompt("Enter your username:");
+// if (username.indexOf("@") >= 0 || username.indexOf(".") >= 0 || username.indexOf(",") >= 0 || username.indexOf("!") >= 0) {
+//   console.log("Please enter a valid username");
+// } else {
+//   var validUsername = username;
+//   console.log("Valid username:", validUsername);
+// }
+
+//Question 14
+
+// var array=["cake","apple pie","cookie","chips","patties"];
+
+// var userInput=prompt("Enter your input");
+// var found=false;
+// for(var i=0;i<array.length;i++){
+//     if(array[i].toLowerCase()===userInput.toLowerCase()){
+//        found=true;
+//        break;
+// }
+// }
+// if(found){
+//     console.log("it is available in the list")
+// }else{
+//     console.log("It is not available in the list")
+// }
+
+//Question 15
+
+// var password=prompt("Enter your password");
+// if(password.length>=6){
+//     console.log("the password is Valid")
+// }
+// var firstLetter=password.charCodeAt(0);
+// if((firstLetter<=65&&firstLetter>=90)&&(firstLetter<=97&&firstLetter>=122)){
+// console.log("the password is valid")
+// }
+
+
+
+
+
+
+
+
+//Question 16
+
+// var university="University of karachi";
+// var newArray=(university.split(" "))
+// document.write(newArray);
+
+//Question 17
+
+// var userInput="Pakistan"
+// var newInput=userInput.charAt(7)
+// console.log(newInput)
